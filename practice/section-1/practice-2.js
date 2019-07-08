a5e7e68a3aada6dd6b6b3a43f63f80ae430b809f
ALi816GX <854501715@qq.com>
2019-07-08 11:52:00 +0800

    Update practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+
+  const collectionC = [];
+  for(i = 0;i < collectionA.length;i++){
+    for(j = 0;j < collectionB.length;j++){
+      for(k = 0;k < collectionB[j].length;k++){
+      if(collectionA[i] == collectionB[j][k]){
+        collectionC.add(collectionA[i]);
+        continue;
+        }
+      }
+    }
+  }
+
+  return collectionC;
 }
